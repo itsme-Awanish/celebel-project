74ddf5d60eb09c1fed9a6495e85832408547664e
itsme-Awanish <kawanish387@gmail.com>
2023-07-24 17:03:10 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+//PLEASE EDIT YOUR MONGO DB CONNECTION HERE
+/*YOU CAN FIND SAMPLE COLLECTION at mongodb_collections folder */
+var mongoURL =
+  "mongodb+srv://root:root@cluster0.9p38rmq.mongodb.net/mern-rooms";
+
+mongoose.connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true });
+
+var connection = mongoose.connection;
+
+connection.on("error", () => {
+  console.log("Mongo DB Connection Failed");
+});
+
+connection.on("connected", () => {
+  console.log("Mongo DB Connection Successful");
+});
+
+module.exports = mongoose;
